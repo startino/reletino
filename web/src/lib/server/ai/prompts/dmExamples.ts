e94d1590e037b4e373ec99c561f0df0e37e07b60
Biwas Bhandari <biwas2059@gmail.com>
2024-08-07 14:12:33 +0545

    fix(supabase): database types and imports

--- web/src/lib/server/ai/prompts/dmExamples.ts
+++ web/src/lib/server/ai/prompts/dmExamples.ts
@@ -1,115 +1,115 @@
-export const dmExamples: string = ```
-Heya. Saw your post regarding the workflow AI chatbot.
+export const dmExamples: string = `
+    Heya. Saw your post regarding the workflow AI chatbot.
 
-I'm Jorge, I run a software development firm that focuses on AI SaaS projects. I understand you probably aren't looking to pay someone to build this for you as you're trying to do it using no-code, and I wanted to lend a hand regardless. I'm hoping I could help define further what you're trying to build then could recommend some things as a two cent with my experience as a tech person.
+    I'm Jorge, I run a software development firm that focuses on AI SaaS projects. I understand you probably aren't looking to pay someone to build this for you as you're trying to do it using no-code, and I wanted to lend a hand regardless. I'm hoping I could help define further what you're trying to build then could recommend some things as a two cent with my experience as a tech person.
 
-Biggest questin off the bat: what type of workflows do you mean?
-Hey. Saw your post on your v1 to v2 plans for your SaaS and how you're looking for someone to help out with the tech stuff.
+    Biggest questin off the bat: what type of workflows do you mean?
+    Hey. Saw your post on your v1 to v2 plans for your SaaS and how you're looking for someone to help out with the tech stuff.
 
-I'm Jorge, I run a software consultancy that helps non-tech entrepreneurs plan and develop their software products.
+    I'm Jorge, I run a software consultancy that helps non-tech entrepreneurs plan and develop their software products.
 
-If I were you, I would try to hire in-house, if you're funding doesn't allow for that, second best option is outsourcing to a good freelancer, try to network and get a referral, really a game of luck without one, even with strong vetting. Just being totally honest, hiring an agency is usually the least ideal option. Usually meaning both fundamentally how we operate but also the terrible history development agencies have lol.
+    If I were you, I would try to hire in-house, if you're funding doesn't allow for that, second best option is outsourcing to a good freelancer, try to network and get a referral, really a game of luck without one, even with strong vetting. Just being totally honest, hiring an agency is usually the least ideal option. Usually meaning both fundamentally how we operate but also the terrible history development agencies have lol.
 
-I'll drop our links below in case you'd like to take a look. I'm more than happy to help regardless if we work officially together or not to get your v2 started. Whether its helping vett your more fitting candidates or giving two cents on tech stuff. Let me know.
+    I'll drop our links below in case you'd like to take a look. I'm more than happy to help regardless if we work officially together or not to get your v2 started. Whether its helping vett your more fitting candidates or giving two cents on tech stuff. Let me know.
 
-Website: https://starti.no
-My LinkedIn: https://www.linkedin.com/in/jorge-lewis/
-Discord username: horheyjorge
-Email: jorge.lewis@starti.no
+    Website: https://starti.no
+    My LinkedIn: https://www.linkedin.com/in/jorge-lewis/
+    Discord username: horheyjorge
+    Email: jorge.lewis@starti.no
 
----
+    ---
 
-hey, saw your post about your travel planning idea.
+    hey, saw your post about your travel planning idea.
 
-wanted to offer a helping hand for tech-related things. No- it's not free haha. I want to be paid with experience and learning how I can better help our current and future clients. Some background on me: I'm Jorge. been programming for 6 years. Started a software consultancy for startups and entrepreneurs half a year ago. I could give a second set of eyes on things like your plans and potential hires, and give my two cents. Excited to see how I could help.
+    wanted to offer a helping hand for tech-related things. No- it's not free haha. I want to be paid with experience and learning how I can better help our current and future clients. Some background on me: I'm Jorge. been programming for 6 years. Started a software consultancy for startups and entrepreneurs half a year ago. I could give a second set of eyes on things like your plans and potential hires, and give my two cents. Excited to see how I could help.
 
-Cheers,
-Jorge
+    Cheers,
+    Jorge
 
----
+    ---
 
-hey, saw your post about your influencer content grouping platform.
+    hey, saw your post about your influencer content grouping platform.
 
-wanted to offer a helping hand for tech-related things. No- it's not free haha. I want to be paid with experience and learning how I can better help our current and future clients. Some background on me: I'm Jorge. been programming for 6 years. Started a software consultancy for startups and entrepreneurs half a year ago. I could give a second set of eyes and two cents on things like your plans and potential hires. Excited to see how I could help.
+    wanted to offer a helping hand for tech-related things. No- it's not free haha. I want to be paid with experience and learning how I can better help our current and future clients. Some background on me: I'm Jorge. been programming for 6 years. Started a software consultancy for startups and entrepreneurs half a year ago. I could give a second set of eyes and two cents on things like your plans and potential hires. Excited to see how I could help.
 
-Cheers,
-Jorge
+    Cheers,
+    Jorge
 
----
+    ---
 
-Heya. Saw your post regarding the music practice app.
+    Heya. Saw your post regarding the music practice app.
 
-I'm Jorge, I run a software development firm that focuses on SaaS projects for entrepreneurs. I'm not sure if you're trying to pay for this to get built, and I wanted to lend a hand regardless. I'm hoping I could help define further what you're trying to build then could recommend some things as a two cent with my experience as a tech person.
+    I'm Jorge, I run a software development firm that focuses on SaaS projects for entrepreneurs. I'm not sure if you're trying to pay for this to get built, and I wanted to lend a hand regardless. I'm hoping I could help define further what you're trying to build then could recommend some things as a two cent with my experience as a tech person.
 
-Cheers,
-Jorge
+    Cheers,
+    Jorge
 
----
-Heya. Saw your post regarding your referral program idea.
+    ---
+    Heya. Saw your post regarding your referral program idea.
 
-I'm Jorge, I run a software development firm that focuses on SaaS projects. I'm not sure if you're trying to pay for this to get built, and I wanted to lend a hand regardless. I'm hoping I could help define further what you're trying to build then could recommend some things as a two cent with my experience as a tech person.
+    I'm Jorge, I run a software development firm that focuses on SaaS projects. I'm not sure if you're trying to pay for this to get built, and I wanted to lend a hand regardless. I'm hoping I could help define further what you're trying to build then could recommend some things as a two cent with my experience as a tech person.
 
-Cheers,
-Jorge
+    Cheers,
+    Jorge
 
----
+    ---
 
-Heya. Read your post on wanting to get into SaaS in the future. Keen on connecting. Curious why you're choosing Miami or Dubai as a new location?
+    Heya. Read your post on wanting to get into SaaS in the future. Keen on connecting. Curious why you're choosing Miami or Dubai as a new location?
 
 
 
-Here's my LinkedIn: https://www.linkedin.com/in/jorge-lewis/
+    Here's my LinkedIn: https://www.linkedin.com/in/jorge-lewis/
 
----
+    ---
 
-hey, saw your post about your finance software idea.
+    hey, saw your post about your finance software idea.
 
-wanted to offer a helping hand for tech-related things. No- it's not free haha. I want to be paid with experience and learning how I can better help our current and future clients. Some background on me: I'm Jorge. been programming for 6 years. Started a software development firm for executives and entrepreneurs half a year ago. I could give a second set of eyes and two cents on things like your plans and potential tech hires. Excited to see how I can help.
+    wanted to offer a helping hand for tech-related things. No- it's not free haha. I want to be paid with experience and learning how I can better help our current and future clients. Some background on me: I'm Jorge. been programming for 6 years. Started a software development firm for executives and entrepreneurs half a year ago. I could give a second set of eyes and two cents on things like your plans and potential tech hires. Excited to see how I can help.
 
-Cheers,
-Jorge
+    Cheers,
+    Jorge
 
-LinkedIn: https://www.linkedin.com/in/jorge-lewis/
-WhatsApp: +852 9747 3013
-Discord username: horheyjorge
+    LinkedIn: https://www.linkedin.com/in/jorge-lewis/
+    WhatsApp: +852 9747 3013
+    Discord username: horheyjorge
 
----
+    ---
 
-Hey man. Saw your post about your debate whether you should start a SaaS or drone business. I'd like to offer a hand. I've been programming for 6 years and started a software consultancy targeted towards entrepreneurs and executives 6 months ago. If you can see an angle that allows me to offer my 2 cents on different things, like your plan and potential hire, then do let me know. No- it's not free haha. I simply want to be paid with experience and learning how I can better help our current and future clients.
+    Hey man. Saw your post about your debate whether you should start a SaaS or drone business. I'd like to offer a hand. I've been programming for 6 years and started a software consultancy targeted towards entrepreneurs and executives 6 months ago. If you can see an angle that allows me to offer my 2 cents on different things, like your plan and potential hire, then do let me know. No- it's not free haha. I simply want to be paid with experience and learning how I can better help our current and future clients.
 
-Cheers,
-Jorge Lewis
+    Cheers,
+    Jorge Lewis
 
----
+    ---
 
-Heya. Saw your post regarding the niched down CRM idea.
+    Heya. Saw your post regarding the niched down CRM idea.
 
-I'm Jorge, I run a software development firm that focuses on AI SaaS projects. I understand you probably aren't looking to pay someone to build this for you as you're trying to do it using no-code, and I wanted to lend a hand regardless. I'm hoping I could help define further what you're trying to build then could recommend some things as a two cent with my experience as a tech person.
+    I'm Jorge, I run a software development firm that focuses on AI SaaS projects. I understand you probably aren't looking to pay someone to build this for you as you're trying to do it using no-code, and I wanted to lend a hand regardless. I'm hoping I could help define further what you're trying to build then could recommend some things as a two cent with my experience as a tech person.
 
-Cheers,
-Jorge
-hey, saw your post about your case management software idea for non-profits.
+    Cheers,
+    Jorge
+    hey, saw your post about your case management software idea for non-profits.
 
-wanted to offer a helping hand for tech-related things. No- it's not free haha. I want to be paid with experience and learning how I can better help our current and future clients. Some background on me: I'm Jorge. been programming for 6 years. Started a software development firm for entrepreneurs and executives half a year ago. I could give a second set of eyes and two cents on things like your plans and potential hires. Excited to see how I could help.
+    wanted to offer a helping hand for tech-related things. No- it's not free haha. I want to be paid with experience and learning how I can better help our current and future clients. Some background on me: I'm Jorge. been programming for 6 years. Started a software development firm for entrepreneurs and executives half a year ago. I could give a second set of eyes and two cents on things like your plans and potential hires. Excited to see how I could help.
 
-Cheers,
-Jorge
+    Cheers,
+    Jorge
 
----
+    ---
 
-Hey Ramesh. Saw your post regarding your product idea.
+    Hey Ramesh. Saw your post regarding your product idea.
 
-I'm Jorge, been programming for 6 years now, I run a software development firm that builds products for entrepreneurs and industry experts. I'm not sure if you're trying to pay for this to get built as you suggested building it yourself, and I wanted to lend a hand regardless. I'm hoping I could help define further what you're trying to build or vet a potential hire by giving my two cents from my experience as a tech person. 
+    I'm Jorge, been programming for 6 years now, I run a software development firm that builds products for entrepreneurs and industry experts. I'm not sure if you're trying to pay for this to get built as you suggested building it yourself, and I wanted to lend a hand regardless. I'm hoping I could help define further what you're trying to build or vet a potential hire by giving my two cents from my experience as a tech person. 
 
-I saw other people already recommending what I would, which is patenting usually isn't your first worry.
+    I saw other people already recommending what I would, which is patenting usually isn't your first worry.
 
-Cheers,
-Jorge
+    Cheers,
+    Jorge
 
----
+    ---
 
-hey, I saw a comment you posted that you're looking for help building an MVP app. here to offer a helping hand. I bet you got quite a few applications through it as well haha. I'm a dev with 6 years of experience turned businessman. I run a software development firm that helps industry experts with entrepreneurial spirits to build MVPs and prototypes. I'd love to help in any way I can. If you've already found someone, still more than happy to help give my two cents on tech related things.
+    hey, I saw a comment you posted that you're looking for help building an MVP app. here to offer a helping hand. I bet you got quite a few applications through it as well haha. I'm a dev with 6 years of experience turned businessman. I run a software development firm that helps industry experts with entrepreneurial spirits to build MVPs and prototypes. I'd love to help in any way I can. If you've already found someone, still more than happy to help give my two cents on tech related things.
 
-Cheers,
-Jorge Lewis
-```
+    Cheers,
+    Jorge Lewis
+`;
