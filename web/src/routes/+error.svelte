c311a4d091dae61eaff766a7e48c6b42ee8c24d8
antopiahk <antopiahk@gmail.com>
2025-02-06 00:32:13 +0700

    fix(project): edit button fixed, error page takes to dashboard

--- web/src/routes/+error.svelte
+++ web/src/routes/+error.svelte
@@ -21,7 +21,7 @@
 		</p>
 		<div class="mt-10 flex items-center justify-center gap-x-6">
 			<a
-				href="/"
+				href="/dashboard"
 				class="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-primary-foreground shadow-sm hover:bg-primary/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 			>
 				Return Home
