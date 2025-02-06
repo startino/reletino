4bce6174937f8455d671d7824e44d1a548a2b23b
antopiahk <antopiahk@gmail.com>
2025-02-06 19:24:05 +0700

    feat(projects): shortened objective types

--- web/src/routes/(admin)/dashboard/[envSlug]/(menu)/create/index.ts
+++ web/src/routes/(admin)/dashboard/[envSlug]/(menu)/create/index.ts
@@ -21,11 +21,7 @@ export const OBJECTIVES: Objective[] = [
     { label: "Find Leads", value: "find_leads" },
     { label: "Find Competitors", value: "find_competitors" },
     { label: "Find Ideas", value: "find_ideas" },
-    { label: "Find Content", value: "find_content" },
     { label: "Find Influencers", value: "find_influencers" },
     { label: "Find Investors", value: "find_investors" },
     { label: "Find Partners", value: "find_partners" },
-    { label: "Find Suppliers", value: "find_suppliers" },
-    { label: "Find Mentors", value: "find_mentors" },
-    { label: "Find Mentees", value: "find_mentees" },
 ]; 
